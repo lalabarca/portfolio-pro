b72be18c8a55858f5fab7354103168a3495251f2
Laura Hardy-Dessources <laura.hardydes@gmail.com>
2022-01-28 12:54:40 -0400

    fixed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   # skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @skills_design = Skill.where("specialty = ? AND rating = ?", "0", 3);
+    @skills_design = Skill.where("specialty = ? AND rating = ?", "0", 3)
     @skills_web = Skill.where("specialty = ? AND rating = ?", "1", 3)
     @skills_web_standard = Skill.where("specialty = ? AND rating = ?", "1", 2).first
     @projects = Project.order(updated_at: :desc)
