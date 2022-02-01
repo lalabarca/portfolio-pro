1521299ead47a16487f3c3ab07b7d25a351b5367
Laura Hardy-Dessources <laura.hardydes@gmail.com>
2022-02-01 12:17:09 -0400

    commenté ligne skills_list home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,7 @@ class PagesController < ApplicationController
     @skills_design = Skill.where("specialty = ? AND rating = ?", "0", 3)
     @skills_web = Skill.where("specialty = ? AND rating = ?", "1", 3)
     @skills_web_standard = Skill.where("specialty = ? AND rating = ?", "1", 2).first
+
     @projects = Project.order(updated_at: :desc)
   end
 end
