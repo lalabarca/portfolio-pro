01e8bb54cc126a49e8d004c54a331205cb5cf9fe
Laura Hardy-Dessources <laura.hardydes@gmail.com>
2022-08-20 21:33:11 -0400

    link to go up updated

--- app/javascript/packs/to-top.js
+++ app/javascript/packs/to-top.js
@@ -1,13 +1,15 @@
 const toTop = () => {
   const link = document.querySelector('.to-top');
 
-  const scrollBtnDisplay =  () => {
-    window.scrollY > window.innerHeight
-      ? link.style.opacity = 1
-      : link.style.opacity = 0;
-  };
+  if (link) {
+    const scrollBtnDisplay = () => {
+      window.scrollY > window.innerHeight
+        ? link.style.opacity = 1
+        : link.style.opacity = 0;
+    };
 
-  window.addEventListener("scroll", scrollBtnDisplay);
-}
+      window.addEventListener("scroll", scrollBtnDisplay);
+    }
+  }
 
 export { toTop };
