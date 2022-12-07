0dbffa9c46953c43cbcd716e4e925bda7cb1bc66
Laura Hardy-Dessources <laura.hardydes@gmail.com>
2022-12-07 10:07:25 -0400

    resolved an error concerning date_end of projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,7 +2,10 @@ class ProjectsController < ApplicationController
   # skip_after_action :verify_authorized, only: :show
   def show
     @project = Project.find(params[:id])
-    @nb_jours = (@project.date_end - @project.date_start).to_i
+    unless @project.date_end === nil
+      @nb_jours = (@project.date_end - @project.date_start).
+    to_i
+    end
   end
 
   def date_format(date)
