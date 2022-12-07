d42bed70396be747e71513be47227826beb3ceb0
Laura Hardy-Dessources <laura.hardydes@gmail.com>
2022-12-07 09:40:26 -0400

    adding pluralize method for how many days i worked on a project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,10 +2,12 @@ class ProjectsController < ApplicationController
   # skip_after_action :verify_authorized, only: :show
   def show
     @project = Project.find(params[:id])
+    @nb_jours = (@project.date_end - @project.date_start).to_i
   end
 
   def date_format(date)
     date.strftime("%-d/%-m/%Y")
   end
+
   helper_method :date_format
 end
