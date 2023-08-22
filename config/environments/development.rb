339539ab5e8cf36d37cdffd1c157c0c1ddb62d40
Laura Hardy-Dessources <laura.hardydes@gmail.com>
2023-08-22 13:59:56 -0400

    removed all last changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,18 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "localhost:3000", protocol: "http" }
-
-  # SMTP settings for gmail
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    user_name: Rails.application.credentials.gmail.address,
-    password: Rails.application.credentials.gmail.password,
-    authentication: "plain",
-    enable_starttls_auto: true
-  }
+  config.action_mailer.default_url_options = { host: "http://localhost:3000" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,19 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "http://www.ldesign.me" }
-
-  # SMTP settings for gmail
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    user_name: Rails.application.credentials.gmail.address,
-    password: Rails.application.credentials.gmail.password,
-    authentication: "plain",
-    enable_starttls_auto: true
-  }
-  # config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }
+  config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
